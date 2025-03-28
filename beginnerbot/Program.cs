f5937523a87f90a92c0215b05ab7ae106e0b0683
Delta <deltafoxess@gmail.com>
2025-03-28 14:08:23 -0700

    First commit

---
+++ beginnerbot/Program.cs
@@ -0,0 +1,35 @@
+﻿using DSharpPlus;
+
+namespace beginnerbot
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            DotNetEnv.Env.TraversePath().Load();
+
+            string botToken = Environment.GetEnvironmentVariable("BOT_TOKEN");
+            if (string.IsNullOrEmpty(botToken))
+            {
+                Console.WriteLine("Bot token broke.");
+                Environment.Exit(1);
+            }
+
+            DiscordClientBuilder builder = DiscordClientBuilder.CreateDefault(botToken, DiscordIntents.All);
+
+            builder.ConfigureEventHandlers
+            (
+                b => b.HandleMessageCreated(async (s, e) =>
+                {
+                    if (e.Message.Content.ToLower().StartsWith("ping"))
+                    {
+                        await e.Message.RespondAsync("pong!");
+                    }
+                })
+            );
+
+            await builder.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
